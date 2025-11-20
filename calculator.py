270ef5e7ce79e7ed5afbc0e674a5767e4c695573
gawp7 <alilazrag186@gmail.com>
2025-11-20 15:16:12 +0100

    Rajout et amélioration

--- calculator.py
+++ calculator.py
@@ -1,25 +1,30 @@
 from math import *
-message = input("Quel type d'opération veut tu effectuer ? :")
 
-multiplication : "multiplication".strip().lower()
-addition : "addition".strip().lower()
-soustraction : "soustraction".strip().lower()
-division : "division".strip().lower()
+message = input("Quel type d'opération veut tu effectuer ? :").strip().casefold()
+if message not in ["multiplication","addition","soustraction","division"]:
+      print("Veuillez entrez une opération valide")
 
-a = float(input("Entrez votre premier nombre entier :"))
-b = float(input("Entrez votre deuxième nombre entier :"))
+try :
+    a = float(input("Entre le premier nombre :"))
+    b = float(input("Entre le deuxième nombre :"))
+except : 
+    print("Veuillez entrez un nombre entier ou à virgule")
 
-multiplication = a * b
-addition = a + b
-soustraction = a - b
-division = a / b
-if message == ("multiplication" , "addition" , "soustraction" , "division") :
-   print("Erreur, Veuillez entrer une opération valide (multiplication, addition, soustraction, division)")
-if multiplication :
-    print("Le résultat de la multiplication est :", multiplication)
-if addition : 
-    print("Le résultat de l'addition est :", addition)
-if soustraction :
-    print("Le résultat de la soustraction est :", soustraction)
-if division :
-    print("Le résultat de la division est :", division)
+def multiplication(): 
+      return a * b
+def addition() : 
+      return a + b
+def soustraction() : 
+      return a - b
+def division() : 
+      return a / b
+
+if message == "multiplication":
+    print("Le résultat de votre Multiplication:", multiplication())
+elif message == "addition" :
+        print("Le résultat de votre addition :", addition())
+elif message == "soustraction":
+        print("Le résultat de votre soustraction :", soustraction())
+elif message == "division":
+        print("Le résultat de votre division :", division())
+
